6049b07269e7f6112eeb1d65253ffbef8ad1e727
Jaun-Luc <jaunlucbrown15@gmail.com>
2022-03-22 21:38:19 -0500

    trying something

--- app/config.py
+++ app/config.py
@@ -7,7 +7,8 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://zidnvhnqzaoksf:c56fea815819d9360f019fe338e2442f632de61f70007918fce0e8343fc026f8@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d7mcbanutoueft', '').replace('postgres://', 'postgresql://')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://zidnvhnqzaoksf:c56fea815819d9360f019fe338e2442f632de61f70007918fce0e8343fc026f8@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d7mcbanutoueft', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zidnvhnqzaoksf:c56fea815819d9360f019fe338e2442f632de61f70007918fce0e8343fc026f8@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d7mcbanutoueft'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
     
